ae2e6250d2fb5fe70de753d8d51a258a1bc2333c
parkduhwan <enghksqkr05@gmail.com>
2024-11-12 18:26:32 +0900

    fix: userDto 수정

--- src/main/java/com/duhwan/ustime_backend/dto/UserDto.java
+++ src/main/java/com/duhwan/ustime_backend/dto/UserDto.java
@@ -1,7 +1,9 @@
 package com.duhwan.ustime_backend.dto;
 
 import lombok.*;
+import org.springframework.cglib.core.Local;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
@@ -14,16 +16,24 @@ public class UserDto {
     private String email;               //이메일
     private String password;            //비밀번호
     private String name;                //이름
+    private LocalDate birthdate;        //생년월일
+    private String gender;              //성별
+    private String phone;               //전화번호
     private Long coupleId;              //커플 고유 ID
     private LocalDateTime createdAt;    //생성일
 
-    public static UserDto create(String email, String password, String name, Long coupleId) {
+    public static UserDto create(String email, String password, String name, Long coupleId,
+                                 LocalDate birthdate, String gender, String phone) {
         return UserDto.builder()
                 .email(email)
                 .password(password)
                 .name(name)
                 .coupleId(coupleId)
+                .birthdate(birthdate)
+                .gender(gender)
+                .phone(phone)
                 .createdAt(LocalDateTime.now()) // 현재 시간으로 생성일 설정
                 .build();
     }
+
 }
