4789e46842e9ff30f450f5ee716d88bcfadedb83
parkduhwan <enghksqkr05@gmail.com>
2024-11-26 18:05:39 +0900

    fix: cors allowMethods(OPTIONS) 추가

--- src/main/java/com/duhwan/ustime_backend/config/security/SecurityConfig.java
+++ src/main/java/com/duhwan/ustime_backend/config/security/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")  // 모든 경로에 대해 CORS 허용
                         .allowedOrigins("http://localhost:3000","https://www.ustime-backend.store","https://www.ustime.store")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
+                        .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")  // 허용할 HTTP 메서드
                         .allowedHeaders("*")  // 허용할 헤더
                         .allowCredentials(true);  // 쿠키와 같은 자격 증명을 포함
             }
